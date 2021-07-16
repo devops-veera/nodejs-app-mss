a36cbf5e96f67ec72c09e308158e67ab20492b39
veerac-80 <59869613+veerac-80@users.noreply.github.com>
2021-07-16 19:17:39 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://15.207.21.165:9009//',
+  serverUrl: 'http://15.207.21.165:9009',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
